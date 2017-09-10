09b01c7d1a9f6d4858792b7c34a134108699d2ed
mrijvee <mezbahrijvee@gmail.com>
2017-09-10 16:49:48 -0700

    TeamFourTesting

---
+++ Generic/src/main/java/common/CommonAPI.java
@@ -0,0 +1,54 @@
+package common;
+
+import org.apache.xpath.SourceTree;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+class CommmonAPI {
+    public static WebDriver driver = null;
+
+    public void setUp(String browserName, String os, String URL){
+        System.out.println("session is going to start");
+        getLocalDriver(browserName, os);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL);
+        driver.manage().window().maximize();
+    }
+
+    public WebDriver getLocalDriver (String browserName, String os){
+        browserName= browserName.toLowerCase();
+        os = os.toLowerCase();
+
+        if (browserName.contains("chrome")) {
+            if (os.contains("win")) {
+                System.setProperty("webdriver.chrome.driver", "../generic/driver/chromedriver.exe");
+            }else if (os.contains("mac") || os.contains("os x")){
+                System.setProperty("webdriver.chrome.driver", "../generic/driver/chromedriver");
+            }else{
+                System.err.println("ERROR: Invalid driver path");
+            }
+            driver = new ChromeDriver();
+
+        }else if (os.contains("firefox")) {
+            if (os.contains("win")) {
+                System.setProperty("webdriver.gecko.driver", "../generic/driver/geckodriver.exe");
+            }else if (os.contains("mac") || os.contains("os x")) {
+                System.setProperty("webdriver.gecko.driver", "../generic/driver/geckodriver");
+
+            }else {
+                System.err.println("ERROR: Invalid driver path");
+
+            }
+            driver = new FirefoxDriver();
+        }
+        return driver;
+    }
+    public void finishUp(){
+        System.out.println("session is over");
+        driver.close();
+    }
+}
+
